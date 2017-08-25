63eca9ac9cb019a2776f0985891700d0c4290b06
fisker <lionkay@gmail.com>
2017-08-25 15:28:42 +0800

    add info

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * Copyright(c) 2011 Sencha Inc.
  * Copyright(c) 2011 TJ Holowaychuk
  * Copyright(c) 2014-2015 Douglas Christopher Wilson
+ * Copyright(c) 2017 fisker Cheung
  * MIT Licensed
  */
 
